1b8544cfb170b57f725bfa6a04311e9c3fa1b2b6
selfmusing <mirandamehek@gmail.com>
2024-09-28 04:06:17 +0530

    violet: parts: Nuke Dirac
    This reverts commit f3c0a8d038ed67f009ddb9f6159de045f2cd1efa.
    
    Revert "violet: parts: Update Dirac Tile label"
    
    This reverts commit a0dca83b28ccafea04ed2aa7483ee6fb41c56532.
    
    Revert "violet: parts: Add more Dirac presets"
    
    This reverts commit f52ef5dffb334c07904a3e6649392a0b7b1c8491.
    
    Revert "violet: parts: Sync dirac enable string with A12"
    
    This reverts commit 44e0ca9abfefc95a5d44a912395015d76861048d.
    
    Revert "violet: dirac: Adapt to S style"
    
    This reverts commit 92c7be8ad8389983ed03b84136ad4943854d2a4c.
    
    Revert "violet: parts: Introduce Dirac"
    
    This reverts commit b3f12af939068b6faeef3ae26e98935af7c2ebdf.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -34,7 +33,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-	DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
     }
 }
