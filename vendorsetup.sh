f746727a8a81ed0952b33907c09943e2c6aa30ff
rkkratos <rkriyon2@gmail.com>
2024-12-15 02:26:37 +0530

    violet: fix vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/rkkratos/vendor_xiaomi_violet.git -b vic vendor/xiaomi/violet
 git clone --depth=1 https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
-git clone https://gitlab.com/Joker-V2/vendor_xiaomi-firmware_violet.git -b 15 vendor/xiaomi-firmware/violet
+git clone https://gitlab.com/Joker-V2/vendor_xiaomi-firmware_violet.git -b fifteen vendor/xiaomi-firmware/violet
 git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git -b fifteen vendor/MiuiCamera
 git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
 git clone https://github.com/ProjectPixelage/android_hardware_xiaomi.git -b 15 hardware/xiaomi
